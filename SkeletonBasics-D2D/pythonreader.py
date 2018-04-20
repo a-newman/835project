e85014bf7d16a8dc7f52feb8d2f3b8417a2703e2
Anelise Newman <apnewman@mit.edu>
2018-04-19 22:12:55 -0400

    some cleanup stuff

--- SkeletonBasics-D2D/pythonreader.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-while (True): 
-    sleep(1);
-    with open('tmpfifo', 'r') as infile: 
-        lines = infile.readlines()
-        ok = sum([1 for line in lines if "failed" not in line])
-        print(ok)
-        print(len(lines))
