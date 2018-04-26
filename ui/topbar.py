01b91d53296ba800f75f7c660ce688f46454f462
Mubarik <mohamoud@mit.edu>
2018-04-25 23:48:13 -0400

    [adding] feedback and wait states

--- ui/topbar.py
+++ ui/topbar.py
@@ -42,7 +42,7 @@ def processing(size,pos = (0,0)):
   bar.back_color_h = (3,32,11);
   bar.font_color = (r,g,3);
   return bar.show();
-def noData(size,word,pos = (0,0)):
+def noData(size,pos = (0,0)):
   bar =  Button(dims=size,text = "No Data collected!");
   bar.pos = pos;
   bar.back_color_n = (12,12,55);
