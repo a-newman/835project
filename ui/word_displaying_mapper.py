ff1a0b61415f919eb1a5da598304d892e1c923b0
Mubarik <mohamoud@mit.edu>
2018-05-06 17:32:17 -0400

    adding the translation in the feedback

--- ui/word_displaying_mapper.py
+++ ui/word_displaying_mapper.py
@@ -14,7 +14,7 @@ def test__vocab_display_mapper(obj):
 def sorry_bar_mapper(obj):
   disp_word_map = {}
   for word in obj.wordlist.arr:
-    word_bar = bars.sorry(obj.feedback_bar_size,word[0],pos = obj.feedback_bar_pos)
+    word_bar = bars.sorry(obj.feedback_bar_size,word[0]+"("+word[1]+")",pos = obj.feedback_bar_pos)
     disp_word_map[word[0]] = word_bar
   return disp_word_map
 
